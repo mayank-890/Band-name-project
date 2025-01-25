3f1110275e4e6458dd87c9f83c25767a624388f0
Mayank Srivastava <mayank.srivastava.cs28@iilm.edu>
2025-01-25 15:37:53 +0530

    Add files via upload

---
+++ Band_name_generator_project.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name generator project")
+city = input("Which city you are from?\n ")
+pet_name = input("What is your pet's name?\n ")
+print("Your band name could be: " + city + " " + pet_name)
